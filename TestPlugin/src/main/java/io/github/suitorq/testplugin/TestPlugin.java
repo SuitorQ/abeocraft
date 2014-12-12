ceec627fe7ecf9f8c5fb6712b00daf38308e5334
SuitorQ <qs-ds@hotmail.com>
2014-12-12 09:18:25 -0500

    Updated

---
+++ TestPlugin/src/main/java/io/github/suitorq/testplugin/TestPlugin.java
@@ -0,0 +1,15 @@
+package io.github.suitorq.testplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TestPlugin extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		// TODO Insert logic to be performed when the plugin is enabled
+	}
+
+	@Override
+	public void onDisable() {
+		// TODO Insert logic to be performed when the plugin is disabled
+	}
+}
